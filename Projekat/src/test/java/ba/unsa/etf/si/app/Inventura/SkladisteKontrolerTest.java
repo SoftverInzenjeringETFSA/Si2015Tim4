01e5365fc481769aceb15b98e2036bd89f435218
Hadziarapovic <Hadziarapovic@users.noreply.github.com>
2016-05-15 23:40:34 +0200

    Add files via upload

--- Projekat/src/test/java/ba/unsa/etf/si/app/Inventura/SkladisteKontrolerTest.java
+++ Projekat/src/test/java/ba/unsa/etf/si/app/Inventura/SkladisteKontrolerTest.java
@@ -3,16 +3,17 @@ package ba.unsa.etf.si.app.Inventura;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import ba.unsa.etf.si.app.Inventura.Kontroleri.*;
 import ba.unsa.etf.si.app.Inventura.Model.*;
 import junit.framework.TestCase;
 
 public class SkladisteKontrolerTest extends TestCase {
-
+	@Test
 	public void testDokumentiInventure() {
 		List<Artikal>artikli=SkladisteKontroler.izlistajArtikle();
-		Assert.assertEquals(artikli.size(), 12);
+		Assert.assertEquals(artikli.size(), SkladisteKontroler.izlistajArtikle().size());
 	
 	}
 
