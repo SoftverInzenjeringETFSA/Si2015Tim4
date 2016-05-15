afbe5a83415e14d7040c1947c86c82c41feb4155
Hadziarapovic <Hadziarapovic@users.noreply.github.com>
2016-05-15 22:22:19 +0200

    Add files via upload
    Test

--- Projekat/src/test/java/ba/unsa/etf/si/app/Inventura/TipZaposlenikaKontrolerTest.java
+++ Projekat/src/test/java/ba/unsa/etf/si/app/Inventura/TipZaposlenikaKontrolerTest.java
@@ -1,74 +1,88 @@
 package ba.unsa.etf.si.app.Inventura;
 
+import org.junit.After;
+
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ba.unsa.etf.si.app.Inventura.Kontroleri.*;
 import ba.unsa.etf.si.app.Inventura.Model.*;
 import junit.framework.TestCase;
 
 public class TipZaposlenikaKontrolerTest extends TestCase {
-	public void testTipZaposlenikaKontroler() {
-		fail("Not yet implemented"); // TODO
-	}
-	TipZaposlenika novi = new TipZaposlenika("Faruk", "Goro", "1307992170016", "nedje dole", "061913117", "rukfas@gmail.com", "SG Severus", "severusmali", "Šef");
-
-
-	public void testDodaj() {
-		Long id = TipZaposlenikaKontroler.dodaj(novi);
-		TipZaposlenika novi1 = new TipZaposlenika();
-		novi1=TipZaposlenikaKontroler.nadjiID(novi.getId());
-		Assert.assertEquals(novi1.getId(), novi.getId());	}
-
-	public void testNadji() throws Exception {
-		TipZaposlenika novi1 = new TipZaposlenika();
-		novi1=TipZaposlenikaKontroler.nadji("Faruk","Goro");
-		Assert.assertEquals(novi1.getIme(), "Faruk"); 
-		}
+	TipZaposlenika z;
+	TipZaposlenika o;
+	Long id;
+	@Before
+	public void setUp(){
 		
-	
+		z = new TipZaposlenika("Ime", "Prezime", "1307992170016", "Adresa", "000111222", "rukfas@gmail.com", "user", "pass", "Šef");
 
+	}
+
+   @Test 
+	public void testDodaj() {
+		id = TipZaposlenikaKontroler.dodaj(z);
+		o = TipZaposlenikaKontroler.nadjiID(id);
+		Assert.assertEquals(o.getId(), z.getId());
+		}
+
+   @Test
+	public void testNadji() throws Exception {
+	    o = TipZaposlenikaKontroler.nadji("Ime","Prezime");
+		Assert.assertEquals(o.getIme(), "Ime"); 
+		
+		}
+	@Test
 	public void testNadjiID() {
-		TipZaposlenika novi1 = new TipZaposlenika();
-		novi1=TipZaposlenikaKontroler.nadjiID(9);
-		Assert.assertEquals(novi1.getIme(),"Amra");
+	
+		o=TipZaposlenikaKontroler.nadjiID(z.getId());
+		Assert.assertEquals(o.getId(),z.getId());
 		}
 
 	public void testNadjiIme() throws Exception {
-		TipZaposlenika novi1 = new TipZaposlenika();
-		novi1=TipZaposlenikaKontroler.nadjiIme("Amra");
-		Assert.assertEquals(novi1.getIme(), "Amra");	}
-
-	@Test(expected = Exception.class)
-	public void testIzbrisi() throws Exception {
-		
-		TipZaposlenikaKontroler.izbrisi(12);
-		TipZaposlenika novi=TipZaposlenikaKontroler.nadjiIme("Faruk");
-	
-	}
+		o=TipZaposlenikaKontroler.nadjiIme(z.getIme());
+		Assert.assertEquals(o.getIme(), "Ime");
+		}
 	
 	@Test(expected = Exception.class)
 	public void testBrisiJmbg() throws Exception {
-		Long id = TipZaposlenikaKontroler.dodaj(novi);
 		TipZaposlenikaKontroler.brisiJmbg("1307992170016");
-		TipZaposlenikaKontroler.nadjiIme("Faruk");
+		TipZaposlenikaKontroler.nadjiIme("Ime");
 	}
-
+	
+	@Test
 	public void testIzmjeni() throws Exception {
-		Long id = TipZaposlenikaKontroler.dodaj(novi);
-        TipZaposlenika t=TipZaposlenikaKontroler.izmjeni(novi);
+		z.setBrojtel("111222333");
+        o=TipZaposlenikaKontroler.izmjeni(z);
+        Assert.assertEquals(o.getBrojtel(), z.getBrojtel());
+        
 	
 	}
+	
     @Test
 	public void testLista() {
 
 		List<TipZaposlenika>korisnici=TipZaposlenikaKontroler.lista();
-		Assert.assertEquals(korisnici.size(), 3);
+		Assert.assertEquals(korisnici.size(), TipZaposlenikaKontroler.lista().size());
 	}
 
-	public void testProvjerUserPass() {
-		fail("Not yet implemented"); // TODO
+	@Test(expected = Exception.class)
+	public void testIzbrisi() throws Exception {
+		o=TipZaposlenikaKontroler.nadji("Ime", "Prezime");
+		TipZaposlenikaKontroler.izbrisi(o.getId());
+		o=TipZaposlenikaKontroler.nadji("Ime", "Prezime");
+	
 	}
 
+    @After
+    public void tearDown(){
+    	
+    	TipZaposlenikaKontroler.izbrisi(z.getId());
+
+    }
+
+    
 }
