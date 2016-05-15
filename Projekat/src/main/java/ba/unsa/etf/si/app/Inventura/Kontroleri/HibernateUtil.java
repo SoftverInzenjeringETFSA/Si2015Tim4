fa6cdffd2b3b76fa3d289c4f6b130b27bf86521f
sibrahimpa1 <sibrahimpa1@etf.unsa.ba>
2016-05-15 16:33:57 +0200

    testovi fix

--- Projekat/src/main/java/ba/unsa/etf/si/app/Inventura/Kontroleri/HibernateUtil.java
+++ Projekat/src/main/java/ba/unsa/etf/si/app/Inventura/Kontroleri/HibernateUtil.java
@@ -3,26 +3,23 @@ import ba.unsa.etf.si.app.Inventura.Kontroleri.HibernateUtil;
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+ 
 import ba.unsa.etf.si.app.Inventura.GUI.LogInScreen;
-
+ 
 public class HibernateUtil {
-	final static Logger logger = Logger.getLogger(LogInScreen.class);
-	  private static final SessionFactory sessionFactory = buildSessionFactory();
-
-	    @SuppressWarnings("deprecation")
-		private static SessionFactory buildSessionFactory() {
-	        try {
-	            // Create the SessionFactory from hibernate.cfg.xml
-	            return new Configuration().configure().buildSessionFactory();
-	        }
-	        catch (Exception ex) {
-	            logger.info(ex);
-	            throw new ExceptionInInitializerError(ex);
-	        }
-	    }
-
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
+    private static final SessionFactory sessionFactory;
+    private static final Logger logger = Logger.getLogger(LogInScreen.class);
+    static{
+             try {  
+                sessionFactory = new Configuration().configure().buildSessionFactory();
+            }
+ 
+            catch (Exception ex) {
+                // logger.info(ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+    }
+        public static SessionFactory getSessionFactory() {
+            return sessionFactory;
+        }
 }
