e8f457ba628f4b729c7705b76580b291a9301e59
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2016-05-23 02:12:40 +0200

    Instalacija

--- Projekat/src/main/java/ba/unsa/etf/si/app/Inventura/Kontroleri/HibernateUtil.java
+++ Projekat/src/main/java/ba/unsa/etf/si/app/Inventura/Kontroleri/HibernateUtil.java
@@ -1,21 +1,29 @@
 package ba.unsa.etf.si.app.Inventura.Kontroleri;
-import ba.unsa.etf.si.app.Inventura.Kontroleri.HibernateUtil;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
- 
+
 import ba.unsa.etf.si.app.Inventura.GUI.LogInScreen;
+
+
+
+
  
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+    public static final SessionFactory sessionFactory=buildSessionFactory();
     private static final Logger logger = Logger.getLogger(LogInScreen.class);
-    static{
-             try {  
-                sessionFactory = new Configuration().configure().buildSessionFactory();
-            }
+    private static SessionFactory buildSessionFactory(){
+             try { 
+            	 java.util.Properties properties = new Properties();
+            	 properties.load(new FileInputStream("db.properties"));
+            	 return new Configuration().addProperties(properties).configure().buildSessionFactory();
+             }
  
             catch (Exception ex) {
-                // logger.info(ex);
+                logger.info(ex);
                 throw new ExceptionInInitializerError(ex);
             }
     }
